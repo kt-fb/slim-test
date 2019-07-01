b6ddfb40572727230a534389ebca438b3daa6708
Karthik <me@kt12.in>
2019-07-01 10:29:58 +0530

    Fast Route issue fixed

--- src/blogs.php
+++ src/blogs.php
@@ -6,21 +6,25 @@ use Slim\Http\Response;
 
 require_once dirname(__FILE__).'/models/blogs.php';
 
-$app->post('/blogs/id/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
-    return $response->withJson(get_blog_by_id($args['id']));
+$app->post('/blogs/category/[{id}]', function (Request $request, Response $response, array $args) use ($container) {    
+    return $response->withJson(get_blog_list_by_cat_id( $args['id'], $request->getParsedBody()));
 });
 
-$app->post('/blogs/', function (Request $request, Response $response, array $args) use ($container) {
-    return $response->withJson(get_blog_list($request->getParsedBody()));
+$app->post('/blogs/id/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
+    return $response->withJson(get_blog_by_id($args['id']));
 });
 
 $app->post('/blogs/category', function (Request $request, Response $response, array $args) use ($container) {    
     return $response->withJson(get_all_blog_category());
 });
 
-$app->post('/blogs/category/[{id}]', function (Request $request, Response $response, array $args) use ($container) {    
-    return $response->withJson(get_blog_list_by_cat_id( $args['id'], $request->getParsedBody()));
+$app->post('/blogs/', function (Request $request, Response $response, array $args) use ($container) {
+    return $response->withJson(get_blog_list($request->getParsedBody()));
 });
+
+
+
+
 
 
 // for get use  $request->postQueryParams() to get array
--- src/businesses.php
+++ src/businesses.php
@@ -6,35 +6,38 @@ use Slim\Http\Response;
 
 require_once dirname(__FILE__).'/models/businesses.php';
 
-$app->post('/businesses/', function (Request $request, Response $response, array $args) use ($container) {
-    return $response->withJson(get_all_businesses($request->getParsedBody()));
-});
+
 
 $app->post('/businesses/id/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
     return $response->withJson(get_business_by_id($args['id']));
 });
+$app->post('/businesses/', function (Request $request, Response $response, array $args) use ($container) {
+    return $response->withJson(get_all_businesses($request->getParsedBody()));
+});
 
-$app->post('/businesses/categories', function (Request $request, Response $response, array $args) use ($container) {
-    return $response->withJson(get_all_business_categories($request->getParsedBody()));
+
+$app->post('/businesses/category/subcategories/cat-id/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
+    return $response->withJson(get_business_subcategories_by_category_id($args['id'],$request->getParsedBody()));
 });
 $app->post('/businesses/categories/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
     return $response->withJson(get_businesses_by_category_id($args['id'],$request->getParsedBody()));
 });
+$app->post('/businesses/categories', function (Request $request, Response $response, array $args) use ($container) {
+    return $response->withJson(get_all_business_categories($request->getParsedBody()));
+});
 
 
-$app->post('/businesses/category/[{id}]/subcategories', function (Request $request, Response $response, array $args) use ($container) {
-    return $response->withJson(get_business_subcategories_by_category_id($args['id'],$request->getParsedBody()));
+$app->post('/businesses/subcategories/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
+    return $response->withJson(get_all_businesses_by_subcategory_id($args['id'],$request->getParsedBody()));
 });
 
 $app->post('/businesses/subcategories', function (Request $request, Response $response, array $args) use ($container) {
     return $response->withJson(get_all_business_subcategories($request->getParsedBody()));
 });
-$app->post('/businesses/subcategories/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
-    return $response->withJson(get_all_businesses_by_subcategory_id($args['id'],$request->getParsedBody()));
-});
-$app->post('/businesses/cities', function (Request $request, Response $response, array $args) use ($container) {
-    return $response->withJson(get_all_business_cities($request->getParsedBody()));
-});
 $app->post('/businesses/cities/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
     return $response->withJson(get_all_business_cities($args['id'],$request->getParsedBody()));
+});
+
+$app->post('/businesses/cities', function (Request $request, Response $response, array $args) use ($container) {
+    return $response->withJson(get_all_business_cities($request->getParsedBody()));
 });
