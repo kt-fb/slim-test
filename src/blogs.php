cbf5943041cd3e11e0b8378d5006f44261404d50
Karthik <me@kt12.in>
2019-07-01 09:54:48 +0530

    Removing Routes

--- src/blogs.php
+++ src/blogs.php
@@ -6,19 +6,19 @@ use Slim\Http\Response;
 
 require_once dirname(__FILE__).'/models/blogs.php';
 
-$app->post('/api/blogs/id/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
+$app->post('/blogs/id/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
     return $response->withJson(get_blog_by_id($args['id']));
 });
 
-$app->post('/api/blogs/', function (Request $request, Response $response, array $args) use ($container) {
+$app->post('/blogs/', function (Request $request, Response $response, array $args) use ($container) {
     return $response->withJson(get_blog_list($request->getParsedBody()));
 });
 
-$app->post('/api/blogs/category', function (Request $request, Response $response, array $args) use ($container) {    
+$app->post('/blogs/category', function (Request $request, Response $response, array $args) use ($container) {    
     return $response->withJson(get_all_blog_category());
 });
 
-$app->post('/api/blogs/category/[{id}]', function (Request $request, Response $response, array $args) use ($container) {    
+$app->post('/blogs/category/[{id}]', function (Request $request, Response $response, array $args) use ($container) {    
     return $response->withJson(get_blog_list_by_cat_id( $args['id'], $request->getParsedBody()));
 });
 
